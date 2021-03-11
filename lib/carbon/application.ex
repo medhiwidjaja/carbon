15da7afe03677f9c706b4c8ec5b8bd83a400f6a2
Medhi Widjaja <medhiwidjaja@yahoo.com>
2021-03-11 14:22:07 +0700

    Initial commit

---
+++ lib/carbon/application.ex
@@ -0,0 +1,20 @@
+defmodule Carbon.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Carbon.Worker.start_link(arg)
+      # {Carbon.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Carbon.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
