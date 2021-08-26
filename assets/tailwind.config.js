be265a9ce94b9d4b2b66606543ca16aa26c73300
Medhi Widjaja <medhiwidjaja@yahoo.com>
2021-08-26 22:48:38 +0700

    Add Tailwind CSS

---
+++ assets/tailwind.config.js
@@ -0,0 +1,48 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "../lib/**/*.eex",
+      "../lib/**/*.leex",
+      "../lib/**/*_view.ex",
+      './js/**/*.js'
+    ],
+    options: {
+      whitelist: [/phx/, /nprogress/]
+    }
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ['Nunito', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  variants: {
+    extend: {
+      borderColor: ['focus-visible'],
+      opacity: ['disabled'],
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
