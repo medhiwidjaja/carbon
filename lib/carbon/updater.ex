f4c00e4894e1590caaae63b2ad61b4d0a1d9ea6f
Medhi Widjaja <medhiwidjaja@yahoo.com>
2021-08-27 17:38:15 +0700

    FIX Updater with get_latest

--- lib/carbon/updater.ex
+++ lib/carbon/updater.ex
@@ -2,6 +2,8 @@ defmodule Carbon.Updater do
   use GenServer
   require Logger
 
+  alias Carbon.Measure
+
   @me __MODULE__
   @table :carbon
   @urlbase Application.get_env(:carbon, :urlbase)
@@ -11,7 +13,6 @@ defmodule Carbon.Updater do
   def start_link(_opts \\ []) do
     GenServer.start_link(__MODULE__, %{}, name: @me)
   end
-
 
   ## Server Callbacks
 
@@ -48,22 +49,26 @@ defmodule Carbon.Updater do
   end
 
   defp process(fetcher) do
-    {:ok, pid} = Task.await fetcher
+    {:ok, pid} = Task.await(fetcher)
 
-    processor = Task.async((fn -> Carbon.Processor.start(pid) end))
+    processor = Task.async(fn -> Carbon.Processor.start(pid) end)
     Task.await(processor)
   end
 
   defp schedule_download() do
-    latest_record = Carbon.Intensity.get_latest()
+    latest_record = Measure.get_latest()
+
     timestamp =
       case latest_record do
-        nil -> Application.fetch_env!(:carbon, :starting_datetime)
+        nil ->
+          Application.fetch_env!(:carbon, :starting_datetime)
+
         _ ->
           latest_record.to_time
-            |> DateTime.add(Application.fetch_env!(:carbon, :update_delay) * 60)
-            |> DateTime.to_iso8601
+          |> DateTime.add(Application.fetch_env!(:carbon, :update_delay) * 60)
+          |> DateTime.to_iso8601()
       end
+
     schedule_work(timestamp)
     last_saved = if latest_record, do: latest_record.to_time |> DateTime.to_iso8601(), else: nil
     %{timestamp: timestamp, last_saved: last_saved, status: :scheduled}
@@ -72,15 +77,15 @@ defmodule Carbon.Updater do
   defp schedule_work(start_time) do
     now = DateTime.utc_now()
     {:ok, ts, _} = DateTime.from_iso8601(start_time)
-    if DateTime.diff(now, ts)/60 < 30 do
+
+    if DateTime.diff(now, ts) / 60 < 30 do
       delay = 35 - rem(now.minute, 30)
-      IO.puts "Update will be started in #{delay} minutes"
+      IO.puts("Update will be started in #{delay} minutes")
       Process.send_after(self(), :download, delay * 60 * 1000)
     else
-      IO.puts "Update will start immediately"
-      end_time = now |> DateTime.to_iso8601
+      IO.puts("Update will start immediately")
+      end_time = now |> DateTime.to_iso8601()
       Process.send(self(), {:download, start_time, end_time}, [:noconnect])
     end
   end
-
 end
